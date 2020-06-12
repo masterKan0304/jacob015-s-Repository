1bd7b987d884ffebed79346a626900bf2cd417d6
masterKan0304 <66818353+masterKan0304@users.noreply.github.com>
2020-06-12 17:16:01 +0900

    Update AnemyBattle.cs

--- AnemyBattle.cs
+++ AnemyBattle.cs
@@ -20,7 +20,7 @@ public class AnemyBattle : MonoBehaviour
         }
         else if(EnemyTurn == false)
         {
-            //플레이어의 턴이라도 적은 패턴을 통해 플레이어의 공격에 대응할 것
+            //플레이어의 턴이라도 적은 패턴을 통해 플레이어의 공격에 대응할 것ㅋㅋ
         }
     }
 
